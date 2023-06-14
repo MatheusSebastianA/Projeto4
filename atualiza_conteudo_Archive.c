5bc0fb48bb2b62d30ea463d7c265623cbafdce27
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-06-14 12:34:26 -0300

    Update atualiza_conteudo_Archive.c
    Funcionando para valores menores que 1024 e com tamanho maior que o anterior

--- atualiza_conteudo_Archive.c
+++ atualiza_conteudo_Archive.c
@@ -1,8 +1,8 @@
-void atualiza_conteudo(struct diretorio *d, char *nomeArc, char *nomeArq){
+void atualiza_conteudo(struct diretorio *d, char *nomeArq, char *nomeArc){
     char buffer[BUFFER_SIZE] = {0};
-    FILE *archive, *destino;
+    FILE *archive, *arquivo;
     struct nodoM *aux = NULL;
-    int i = 0;
+    int i = 0, blocos, resto;
     long int tam_ant_arq = 0, diferenca = 0, fim = 0, teste = 0;
     struct diretorio *b = cria_diretorio();
     recebe_diretorio(b, "backup.txt", insereI);
@@ -27,8 +27,9 @@ void atualiza_conteudo(struct diretorio *d, char *nomeArc, char *nomeArq){
     fseek(archive, aux->localizacao + tam_ant_arq, SEEK_SET);
 
     if(diferenca > 0){
-        int blocos = (b->inicio_diretorio - ftell(archive)) / BUFFER_SIZE;
-        int resto = (b->inicio_diretorio - ftell(archive)) % BUFFER_SIZE;
+        teste = ftell(archive);
+        blocos = (b->inicio_diretorio - ftell(archive)) / BUFFER_SIZE;
+        resto = (b->inicio_diretorio - ftell(archive)) % BUFFER_SIZE;
         if(blocos >= 1)
             for(blocos = blocos; blocos > 0; blocos--){
                 fread(buffer, sizeof(char), BUFFER_SIZE, archive);
@@ -42,15 +43,35 @@ void atualiza_conteudo(struct diretorio *d, char *nomeArc, char *nomeArq){
             
             fseek(archive, aux->localizacao + aux->tamanho + (blocos * 1024), SEEK_SET);
             fwrite(&buffer, sizeof(char), resto, archive);
+            
+
         }
             
     }
+    arquivo = abre_archive_leitura(nomeArq);
+
+    if(!arquivo)
+        return;
+
+    blocos = aux->tamanho / BUFFER_SIZE;
+    resto = aux->tamanho % BUFFER_SIZE;
+    if(blocos >= 1)
+        for(blocos = blocos; blocos > 0; blocos--){
+            fread(buffer, sizeof(char), BUFFER_SIZE, arquivo);
+            fseek(archive, aux->localizacao, SEEK_CUR);
+            fwrite(buffer, sizeof(char), BUFFER_SIZE, archive);
+        }
+    if(blocos < 1 && resto != 0){
+        for(int i = 0; i < resto; i++)
+            fread(&buffer[i], sizeof(char), 1, arquivo);
+
+        fseek(archive, aux->localizacao + (blocos * 1024), SEEK_SET);
+        fwrite(&buffer, sizeof(char), resto, archive);
+    }    
 
     
     for(int i = aux->ordem; i <= b->fim->ordem - 1; i++){
-        printf("Localização antiga do prox arq: %ld\n", aux->prox->localizacao);
         aux->prox->localizacao = aux->prox->localizacao + diferenca;
-        printf("Localização do prox arq: %ld\n", aux->prox->localizacao);
             
         aux = aux->prox;
     }
