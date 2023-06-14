79001785fdff0fa72d799aa2f8c0a5e47909084c
MatheusSebastianA <101220035+MatheusSebastianA@users.noreply.github.com>
2023-06-13 22:47:32 -0300

    Update libArchive.c

--- libArchive.c
+++ libArchive.c
@@ -1,45 +1,219 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "libArchive.h"
 
+#define _XOPEN_SOURCE_EXTENDED 1
+#define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[]){
-    struct diretorio *d;
-    int opt;
-    while ((opt = getopt(argc, argv, "iam:xrch")) != -1) {
-        switch (opt){
-            case 'i':
-                break;
-            case 'a':
-                break;
-            case 'm':
-                break;
-            case 'x':
-                break;
-            case 'r':
-                break;
-            case 'c':
-                break;
-            case 'h':
-                break;
-            default:
-                printf ("O argumento passado não é válido\n");
-	            return (1);
-        }		
+FILE* abre_archive_leitura_escrita(char *nomeArq){
+    FILE *arq;
+    arq = fopen(nomeArq, "r+b");
+
+    if(arq == NULL)
+        arq = fopen(nomeArq, "wb");
+
+    if(arq == NULL)
+        return NULL;
+    
+    return arq;
+}
+
+FILE* abre_archive_leitura(char *nomeArq){
+    FILE *arq;
+    arq = fopen(nomeArq, "r");
+
+    if(arq == NULL)
+        return NULL;
+    
+    return arq;
+}
+
+struct diretorio* recebe_diretorio(struct diretorio *d, char *nomeArc,  struct nodoM* (* func) (struct nodoM *aux, char *nomeArq)){
+    long int ini_dir = 0, fim = 0;
+    struct nodoM *aux = NULL;
+    FILE *arc = NULL;
+    arc = abre_archive_leitura_escrita(nomeArc);   
+
+    if(!arc)
+        return NULL;
+
+    fread(&ini_dir, sizeof(long int), 1, arc);
+    fseek(arc, 0, SEEK_END);
+    fim = ftell(arc);
+    fseek(arc, ini_dir, SEEK_SET);
+    
+    while(fim - ftell(arc) != 0){
+        if(!(aux = malloc(sizeof(struct nodoM))))
+            return NULL;
+
+        fread(&aux->tam_nome, sizeof(int), 1, arc);
+        fread(aux->nomeArq,sizeof(char), aux->tam_nome, arc);
+        fread(&aux->data, sizeof(time_t), 1, arc);
+        fread(&aux->localizacao, sizeof(long int), 1, arc);
+        fread(&aux->ordem, sizeof(int), 1, arc);
+        fread(&aux->permissoes, sizeof(mode_t), 1, arc);
+        fread(&aux->tamanho, sizeof(off_t), 1, arc);
+        fread(&aux->uid, sizeof(uid_t), 1, arc);
+
+        if(diretorio_vazio(d)){
+
+            if(!(d->inicio = malloc(sizeof(struct nodoM))))
+                return NULL;
+            d->inicio = aux;
+            d->inicio->prox = NULL;
+            d->inicio_diretorio = 0;
+            d->fim = d->inicio;
+        }
+
+        else{
+            if(!(d->fim->prox = malloc(sizeof(struct nodoM))))
+                return NULL;
+            d->fim->prox = aux;
+            d->fim = aux;
+            d->inicio_diretorio = 0;
+        }
+
+
+        aux = aux->prox;
+    }
+    fclose(arc);
+
+    return d;
+}
+
+void insere_diretorio(struct diretorio *d, char *nomeArc){
+    struct nodoM *aux = NULL;
+    FILE *arc = abre_archive_leitura_escrita(nomeArc);   
+
+    if(!arc)
+        return;
+        
+    aux = d->inicio;
+    fseek(arc, d->inicio_diretorio, SEEK_SET);
+
+    for(int i = 0; i <= d->fim->ordem; i++){
+        fwrite(&aux->tam_nome, sizeof(int), 1, arc);
+        fwrite(aux->nomeArq, strlen(aux->nomeArq), 1, arc);
+        fwrite(&aux->data, sizeof(time_t), 1, arc);
+        fwrite(&aux->localizacao, sizeof(long int), 1, arc);
+        fwrite(&aux->ordem, sizeof(int), 1, arc);
+        fwrite(&aux->permissoes, sizeof(mode_t), 1, arc);
+        fwrite(&aux->tamanho, sizeof(off_t), 1, arc);
+        fwrite(&aux->uid, sizeof(uid_t), 1, arc);
+
+        aux = aux->prox;
     }
 
-    d = cria_diretorio();
-    insere_conteudo(d, "teste1.txt", "backup.txt", insereI);
-    insere_conteudo(d, "teste2.txt", "backup.txt", insereI);
-    insere_conteudo(d, "teste3.txt", "backup.txt", insereI);
-    insere_diretorio(d, "backup.txt");
+    return;
+}
 
+int insere_conteudo_menor1024(struct nodoM *nodo, FILE *arq, FILE *archive){
+    char buffer[BUFFER_SIZE] = {0};
+    int tam = 0;
+    int blocos = nodo->tamanho / BUFFER_SIZE;
+    tam = nodo->tamanho % BUFFER_SIZE;
     
-    extrai_conteudo_arquivo("backup.txt", "teste1.txt");
+    for(int i = 0; i < tam; i++)
+        fread(&buffer[i], sizeof(char), 1, arq);
 
+    fseek(archive, nodo->localizacao + (blocos * 1024), SEEK_SET);
+    fwrite(&buffer, sizeof(char), tam, archive);    
 
     return 0;
+}
+
+int insere_bloco_conteudo(struct nodoM *nodo, FILE *arq, FILE *archive){
+    char buffer[BUFFER_SIZE] = {0};
+
+    fread(buffer, sizeof(char), BUFFER_SIZE, arq);
+    fwrite(buffer, sizeof(char), BUFFER_SIZE, archive);
+
+    return 0;
+}
+
+int insere_conteudo(struct diretorio *d, char *nomeArq, char *nomeArc, struct nodoM* (* func) (struct nodoM *aux, char *nomeArq)){
+    int blocos = 0, resto = 0;
+    FILE *arq = NULL, *arc = NULL;
+    struct nodoM *aux = NULL;
+
+    arq = abre_archive_leitura(nomeArq);
+    if(!arq)
+        return 1;
+    
+    arc = abre_archive_leitura_escrita(nomeArc);   
+    if(!arc)
+        return 1;
+
+    if(diretorio_vazio(d)){
+        d->inicio = insere(d, nomeArq, func);
+        fseek(arc, 0, SEEK_SET);
+        fwrite(&d->inicio_diretorio, sizeof(long int), 1, arc);
+        blocos = d->inicio->tamanho / BUFFER_SIZE;
+        resto = d->inicio->tamanho % BUFFER_SIZE;
+        if(blocos >= 1)
+            for(blocos = blocos; blocos > 0; blocos--)
+                insere_bloco_conteudo(d->inicio, arq, arc);
+
+        fseek(arq, 1024*blocos, SEEK_SET);
+        if(blocos < 1 && resto != 0)
+            insere_conteudo_menor1024(d->inicio, arq, arc);
+        
+        fclose(arq);
+        fclose(arc);
+        return 0;
+    }
+    
+    insere(d, nomeArq, func);
+    aux = d->fim;
+    fseek(arc, 0, SEEK_SET);
+    fwrite(&d->inicio_diretorio, sizeof(long int), 1, arc);
+    fseek(arc, d->inicio_diretorio, SEEK_SET);
+    blocos = aux->tamanho / BUFFER_SIZE;
+    resto = aux->tamanho % BUFFER_SIZE;
+    if(blocos >= 1)
+        for(blocos = blocos; blocos > 0; blocos--)
+            insere_bloco_conteudo(aux, arq, arc);
+
+    if(blocos < 1 && resto != 0)
+        insere_conteudo_menor1024(aux, arq, arc);
+    
+
+    fclose(arq);
+    fclose(arc);
+    
+    return 0;
+}
+
+void extrai_conteudo_arquivo(char *arc, char *dest){
+    FILE *archive, *destino;
+    struct nodoM *aux = NULL;
+    struct diretorio *b = cria_diretorio();
+    recebe_diretorio(b, "backup.txt", insereI);
+    imprime_diretorio(b);
+
+    aux = existe_arq(b, dest);
+    if(aux == NULL)
+        return;
+
+    archive = abre_archive_leitura_escrita(arc);
+    destino = abre_archive_leitura_escrita(dest);
+
+    fseek(archive, aux->localizacao, SEEK_SET);
+    aux->localizacao = 0;
+    aux->tamanho = aux->tamanho;
+    int blocos = aux->tamanho / BUFFER_SIZE;
+    int resto = aux->tamanho % BUFFER_SIZE;
+    if(blocos >= 1)
+        for(blocos = blocos; blocos > 0; blocos--)
+            insere_bloco_conteudo(aux, archive, destino);
+
+    if(blocos < 1 && resto != 0)
+        insere_conteudo_menor1024(aux, archive, destino);
+
+    return;
 }
